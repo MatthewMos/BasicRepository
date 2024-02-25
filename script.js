440d10887a86764c3811518633829ba982d278c3
MatthewMos <web.matthewmoskalev@gmail.com>
2024-02-25 23:00:42 +0300

    feat: beta 0.1. Not fucking work!!!

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const butten=document.querySelector('.contacts_button')
-butten.addEventListener('click',()=>alert('Open chat'))
+const butten = document.querySelector(".contacts_button");
+butten.addEventListener("click", () => alert("Open chat"));
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.querySelector('.home_link');
-    const homeSection = document.querySelector('#home');
-    button.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращает перезагрузку страницы
-        homeSection.scrollIntoView({behavior: "smooth"}); // Прокручивает страницу до блока с id "home"
-    });
+const burgerIcon = document.querySelector(".burger"); // получаем кнопку бкргер
+const links = document.querySelector(".header_links"); // получаем ссылки
+burgerIcon.addEventListener("click", () => {
+	// слушатель на клик
+	links.classList.toggle("active"); // логика вкл/выкл класса active
 });
 
+const headerLinks = document.querySelectorAll(".header_links a"); // получаем массив ссылок
+for (let i = 0; i < headerLinks.length; i++) {
+	// бежим по массиву из ссылок
+	const link = headerLinks[i]; // получаем i-й элемент (конкретную ссылку)
+	link.addEventListener("click", () => {
+		// вешаем на каждую i-ю ссылку обработчик клика, чтобы при нажатии на каждую ссылку срабатывала логика
+		links.classList.remove("active"); // убираем класс, чтобы скрыть меню
+		document.documentElement.classList.remove("hide"); // убираем класс, чтобы вернуть скролл
+		burgerIcon.checked = false; // убираем состояние checked, чтобы вернуть бургер в исходное положение
+	});
+}
